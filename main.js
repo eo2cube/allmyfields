6e42ba1bb2f7d0f7762892fc90cf4fed297b0c12
Christoph Friedrich <christoph.friedrich@uni-wuerzburg.de>
2024-08-21 14:00:00 +0200

    Implement time interactivity for WMS layers

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ import Crop from 'ol-ext/filter/Crop';
 import { fromLonLat } from 'ol/proj';
 import Feature from 'ol/Feature';
 import { MultiPolygon } from 'ol/geom';
+import WMSCapabilities from 'ol/format/WMSCapabilities';
+
+let allWMSlayers = [];
+
+const dateslider = document.getElementById('dateslider');
+const datepicker = document.getElementById('datepicker');
 
 const osm = new TileLayer({
   source: new OSM()
@@ -55,6 +61,7 @@ function handleGeoJson(fileContent) {
     const asOneFeature = new Feature(new MultiPolygon([].concat(...coords)));
     // simply take the properties of the first feature (croptype definitely is and year should be the same anyway)
     const properties = matchingFeatures[0].getProperties();
+    datepicker.value = properties.year + '-06-17';
     // create WMS and its mask and add it all to the map
     const wms = new TileLayer({
       source: new TileWMS({
@@ -70,7 +77,34 @@ function handleGeoJson(fileContent) {
     });
     wms.addFilter(crop);
     map.addLayer(wms);
+    allWMSlayers.push(wms);
   }
 
   map.getView().fit(geojson.getSource().getExtent(), {padding:[50,50,50,50]});
+}
+
+window.changeSliderHandler = function (event) {
+  let date = new Date();
+  date.setUTCFullYear(datepicker.value.substr(0, 4));
+  date.setUTCMonth(0); // + '-01-01');  // init with 1st January of the same year
+  date.setUTCDate(dateslider.value);  // set DOY (between 1 and 365)
+  datepicker.value = date.toISOString().substr(0, 10);
+  window.changeDateHandler();
+  /*
+  let date = new Date(datepicker.value.substr(0,4)+'-01-01');  // init with 1st of January of the same year
+  date.setDate(dateslider.value);  // set DOY (between 1 and 365, ignoring leap years)
+  let m = date.getMonth() + 1;  // month is zero-indexed
+  let mm = (m<10 ? '0' : '') + m;
+  let d = date.getDate();
+  let dd = (d<10 ? '0' : '') + d;
+  datepicker.value = year + '-' + mm + '-' + dd;
+  */
+}
+
+window.changeDateHandler = function (event) {
+  setWMSDate(datepicker.value);
+}
+
+function setWMSDate(date) {
+  allWMSlayers.forEach(e => e.getSource().updateParams({'TIME': date + 'T00:00:00Z'}));
 }
